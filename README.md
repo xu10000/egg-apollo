1df1526bed9b6d7f41e08bb84ff6732e97a9c6d7
xzt <qq87315156@gmail.com>
2021-04-09 11:47:47 +0800

    v1.0.0

---
+++ README.md
@@ -0,0 +1,63 @@
+1. 以下配置不能放apollo配置中心
+
+   ```
+   config.cluster
+   
+   config.logger
+   
+   config.keys 
+   
+   config.apollo   
+   ```
+
+2. 引入
+
+   ```
+   npm install @xu10000/egg-apollo
+   ```
+
+3. 使用
+
+   ````
+   // 本地配置文件apollo设置
+     config.apollo = {
+       // 加载远程配置，如果false则仅从本地加载
+       open: true,
+       // 打印本地和远程merge后的配置
+       printKeyValue: true,
+       // apollo参数
+       url: 'http://x.x.x.x:8080/configfiles/json',
+       appId: 'shopping',
+       clusterName: 'local',
+       namespaceName: 'TEST1.book',
+     };
+   ````
+
+   ```
+   // 在app.js文件的生命周期函数configWillLoad使用
+   configWillLoad() {
+       // 打印本地配置，控制台输出的内容可复制到配置中心，避免繁琐的配置
+       const localConfig = require('./config/config.local')({
+         name: 'disable_name',
+         HOME: 'disable_HOME',
+       });
+       apollo.printLocalKeyValue(localConfig);
+       // 从远程加载配置
+       apollo.getRemoteConfig(this.app);
+   }
+   ```
+
+4. 如果配置后不生效，请在配置的对象多加一个app: true, 例子如下：
+
+```
+ config.redis = {
+    client: {
+      port: 6379, // Redis port
+      host: '18.162.188.202', // Redis host
+      password: '',
+      db: 16,
+    },
+    app: true  // 多增加该字段，添加到配置中心
+  };
+```
+
---
+++ lib.js
@@ -0,0 +1,96 @@
+'use strict';
+const request = require('syncrequest');
+const unexpectKeys = [ 'pkg', 'siteFile', 'watcher', 'i18n', 'security', 'bodyParser', 'httpclient', 'session', 'helper', 'jsonp', 'onerror', 'customLogger', 'schedule', 'dump', 'confusedConfigurations', 'notfound', 'appMiddlewares', 'multipartParseOptions', 'meta', 'workerStartTimeout', 'serverTimeout', 'onClientError', 'appMiddleware', 'HOME', 'baseDir', 'hostHeaders', 'ipHeaders', 'protocolHeaders', 'maxIpsCount', 'cookies', 'keys', 'view', 'cookies', 'validate', 'middleware', 'proxy', 'tracer', 'rundir', 'development', 'multipart', 'logrotator', 'maxProxyCount', 'static', 'clusterClient' ];
+const unexpectLocalKeys = [ 'env', 'name', 'logger', 'coreMiddleware', 'cluster', 'apollo', 'coreMiddlewares', 'pkg', 'siteFile', 'watcher', 'i18n', 'security', 'bodyParser', 'httpclient', 'session', 'helper', 'jsonp', 'onerror', 'customLogger', 'schedule', 'dump', 'confusedConfigurations', 'notfound', 'appMiddlewares', 'multipartParseOptions', 'meta', 'workerStartTimeout', 'serverTimeout', 'onClientError', 'appMiddleware', 'HOME', 'baseDir', 'hostHeaders', 'ipHeaders', 'protocolHeaders', 'maxIpsCount', 'cookies', 'keys', 'view', 'cookies', 'validate', 'middleware', 'proxy', 'tracer', 'rundir', 'development', 'multipart', 'logrotator', 'maxProxyCount', 'static', 'clusterClient' ];
+
+module.exports = {
+  getReplaceData(app, data) {
+    // change key-value to json
+
+    for (const key in data) {
+      try {
+        // data[key] = data[key].replace(/\"\{/g, '{')
+        //   .replace(/\}\"/g, '}')
+        //   .replace(/\"\[/g, '[')
+        //   .replace(/\]\"/g, ']');
+        // console.log('\n----xxxxxxxxkey ', key);
+
+        data[key] = JSON.parse(data[key]);
+      } catch (err) {
+        app.logger.error(`apollo远程配置解析key-value出错：key: ${key}, data[key] ${data[key]}`);
+        throw err;
+      }
+    }
+
+    return data;
+  },
+  // 将配置转换成apollo要的key-value格式并打印出来
+  printKeyValue(app, config) {
+    app.logger.info('   ==================  begin print merged apollo keyValue  ====================');
+    for (const key in config) {
+      if (unexpectKeys.includes(key)) {
+        continue;
+      }
+      app.logger.info(`---------key: ${key}\n---------value: ${JSON.stringify(config[key])}`);
+    }
+    app.logger.info('   ==================  end print merged apollo  keyValue  ====================\n\n');
+  },
+  printLocalKeyValue(localConfig) {
+    console.warn('\n\n           ==================  begin print local keyValue====================\n');
+    for (const key in localConfig) {
+      if (unexpectLocalKeys.includes(key)) {
+        continue;
+      }
+      console.warn(`${key}=${JSON.stringify(localConfig[key])}`);
+    }
+    console.warn('\n           ==================  end print local  keyValue  ====================\n\n');
+  },
+  mergeConfig(app, remoteConfig) {
+
+    for (const attr in remoteConfig) {
+      app.config[attr] = remoteConfig[attr];
+    }
+    // 日志输出合并后的config
+    app.logger.debug('获取远程配置成功 '); // ${JSON.stringify(app.config.redis)}
+    if (app.config.apollo.printKeyValue) {
+      this.printKeyValue(app, app.config);
+    }
+    return app.config;
+  },
+  getRemoteConfig(app) {
+    const { apollo } = app.config;
+    try {
+
+      if (!apollo) {
+        throw new Error('-----------配置文件没有app.config.apollo');
+      }
+      
+      if (!apollo.open) {
+        app.logger.info('\n-----------apollo.open not true, config only from local~~~~~');
+        return;
+      }
+      // 同步请求
+      // const result = request.sync('https://www.baidu.com/');
+      const { url, namespaceName, clusterName, appId } = apollo;
+      // const ip = app.config.cluster.listen.hostname;
+      const result = request.sync(`${url}/${appId}/${clusterName}/${namespaceName}`);
+      if (!result.response || result.response.statusCode !== 200 || !result.response.body) {
+        throw new Error('----------请求远程配置失败 ', apollo);
+      }
+
+      if (result) {
+        app.logger.info('\n-----------get remote config success~~~~~');
+      }
+
+      // console.log('\n----xxxxxxxxdatabody ', data);
+
+      const remoteConfig = this.getReplaceData(app, JSON.parse(result.response.body));
+      // !!! 合并本地和远程的配置
+      return this.mergeConfig(app, remoteConfig);
+    } catch (err) {
+      app.logger.error('----------in catch 请求远程配置失败 ', err);
+      process.exit();
+    }
+
+  },
+};
